48bd8b24de7aa9ef1153678a68a8ff0991425e5f
ensKyhn74 <117343032+ensKyhn74@users.noreply.github.com>
2022-11-06 13:56:55 +0300

    Add files via upload

---
+++ for_dongusu_ornek19.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// klavyeden girilen 2 sayının EBOB'unu ve EKOK'unu bulur
+
+int main() 
+
+{
+ 
+ int sayi1,sayi2;
+ int ekok;
+ int i;
+ 
+ printf("1.sayiyi girin: ");
+ scanf("%d",&sayi1);
+  
+ printf("2.sayiyi girin: ");
+ scanf("%d",&sayi2);
+   
+ for(i = (sayi1*sayi2) ; i > 0  ; i--)
+ {
+   if(i % sayi1 == 0 && i % sayi2 == 0)   
+   {  
+    ekok = i;    
+   } 
+ }  
+     
+ int ebob = (sayi1*sayi2) / ekok;
+     
+ printf("EBOB(%d,%d) = %d\n",sayi1,sayi2,ebob);  
+ printf("EKOK(%d,%d) = %d",sayi1,sayi2,ekok);
+
+ return 0;
+
+}
